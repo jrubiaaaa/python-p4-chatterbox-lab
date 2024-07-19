a00d98312d7e4d13a257207bcb3eb2e0b089e657
jrubiaaaa <rubiaj3206@gmail.com>
2024-07-20 01:07:52 +0300

    chchatterbox

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,45 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = []
+        for message in Message.query.order_by('created_at').all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(messages, 200)
+        return response
+    elif request.method == "POST":
+        new_message = Message(
+            body=request.form.get("body"), username=request.form.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        message_dict = new_message.to_dict()
+        response = make_response(message_dict, 201)
+        return response
 
-@app.route('/messages/<int:id>')
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == "PATCH":
+        for attr in request.form:
+            setattr(message, attr, request.form.get(attr))
+        db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+        response = make_response(message_dict, 200)
+        return response
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {"Deleted successfully": True, "message": "Message deleted"}
+        response = make_response(response_body, 200)
+        return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5555)
